6f135f55c7e65129b45feeb236a3f72dc59a9f1c
Kristopher Johnson <kris@kristopherjohnson.net>
2016-05-06 00:01:51 -0400

    Initial revision

---
+++ KJTipCalculatorWPF/TipCalculation.cs
@@ -0,0 +1,15 @@
+﻿namespace KJTipCalculatorWPF
+{
+    class TipCalculation
+    {
+        public decimal Subtotal = 0;
+        public int TipPercentage = 20;
+        public int NumberInParty = 1;
+
+        public decimal Tip => (decimal) (TipPercentage * 0.01 * (double) Subtotal);
+
+        public decimal Total => Subtotal + Tip;
+
+        public decimal PerPerson => (NumberInParty > 1) ? (Total / NumberInParty) : Total;
+    }
+}
---
+++ KJTipCalculatorWPF/ViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using KJTipCalculatorWPF.Annotations;
+
+namespace KJTipCalculatorWPF
+{
+    class ViewModel: INotifyPropertyChanged
+    {
+        private readonly TipCalculation _calc = new TipCalculation();
+
+        public decimal Subtotal
+        {
+            get { return _calc.Subtotal; }
+            set
+            {
+                if (value == _calc.Subtotal) return;
+                _calc.Subtotal = value;
+                OnPropertyChanged(nameof(Subtotal));
+                OnCalcPropertyChanged();
+            }
+        }
+
+        public int TipPercentage
+        {
+            get { return _calc.TipPercentage; }
+            set
+            {
+                if (value == _calc.TipPercentage) return;
+                _calc.TipPercentage = value;
+                OnPropertyChanged(nameof(TipPercentage));
+                OnCalcPropertyChanged();
+            }
+        }
+
+        public int NumberInParty
+        {
+            get { return _calc.NumberInParty; }
+            set
+            {
+                if (value == _calc.NumberInParty) return;
+                _calc.NumberInParty = value;
+                OnPropertyChanged(nameof(NumberInParty));
+                OnCalcPropertyChanged();
+            }
+        }
+
+        public decimal Tip
+        {
+            get { return _calc.Tip; }
+        }
+
+        public decimal Total
+        {
+            get { return _calc.Total; }
+        }
+
+        public decimal PerPerson
+        {
+            get { return _calc.PerPerson; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void OnCalcPropertyChanged()
+        {
+            OnPropertyChanged(nameof(Tip));
+            OnPropertyChanged(nameof(Total));
+            OnPropertyChanged(nameof(PerPerson));
+        }
+    }
+}
